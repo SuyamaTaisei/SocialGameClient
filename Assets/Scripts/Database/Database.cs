ea002441ee97087ad59ccd678b7050c7acff6cb0
SuyamaTaisei <artistplain@gmail.com>
2025-10-30 12:52:46 +0900

    SQLiteの導入
    ・ローカル保存用にSQLiteを導入
    ・フォルダ名を "Script" から "Scripts" に変更

---
+++ Assets/Scripts/Database/Database.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Database : MonoBehaviour
+{
+
+    // Use this for initializationa
+    void Start()
+    {
+        // Insert
+        SqliteDatabase sqlDB = new SqliteDatabase("config.db");
+        string query = "insert into user values(1, 'Yamada', 19, 'Tokyo')";
+        sqlDB.ExecuteNonQuery(query);
+
+        // Select
+        string selectQuery = "select * from user";
+        DataTable dataTable = sqlDB.ExecuteQuery(selectQuery);
+
+        string name = "";
+        foreach (DataRow dr in dataTable.Rows)
+        {
+            name = (string)dr["name"];
+            Debug.Log("name:" + name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

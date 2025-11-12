98b5fcf160cf5fddfc1cf1352a94b29b673ddb60
SuyamaTaisei <artistplain@gmail.com>
2025-11-12 18:21:45 +0900

    [#10655] マスタデータ確認画面の実装
    ・ショップ関連のマスタテーブルの実装

---
+++ Assets/Scripts/Tables/ShopCategoriesTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+[Serializable]
+public class ShopCategoryModel
+{
+    public string category;
+    public string name;
+}
+
+public static class ShopCategoriesTable
+{
+    //テーブル作成
+    public static void CreateTable()
+    {
+        string query = "create table if not exists shop_categories(" +
+            "category string," +
+            "name string," +
+            "primary key(category))";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード挿入
+    public static void Insert(ShopCategoryModel shopCategoryModel)
+    {
+        string query = "insert or replace into shop_categories (" +
+            "category," +
+            "name" +
+            ")" +
+            "values (\"" + shopCategoryModel.category + "\", \"" + shopCategoryModel.name + "\")";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード取得
+    public static ShopCategoryModel Select()
+    {
+        string query = "select * from shop_categories";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        DataTable dataTable = sqlDB.ExecuteQuery(query);
+        ShopCategoryModel shopCategoryModel = new ShopCategoryModel();
+        foreach (DataRow record in dataTable.Rows)
+        {
+            shopCategoryModel.category = record["manage_id"].ToString();
+            shopCategoryModel.name     = record["coin_amount"].ToString();
+        }
+        return shopCategoryModel;
+    }
+}
---
+++ Assets/Scripts/Tables/ShopDataTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+[Serializable]
+public class ShopDataModel
+{
+    public int product_id;
+    public string shop_category;
+    public string name;
+    public int price;
+}
+
+public static class ShopDataTable
+{
+    //テーブル作成
+    public static void CreateTable()
+    {
+        string query = "create table if not exists shop_data(" +
+            "product_id int," +
+            "shop_category string," +
+            "name string," +
+            "price int," +
+            "primary key(product_id))";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード挿入
+    public static void Insert(ShopDataModel shopDataModel)
+    {
+        string query = "insert or replace into shop_data (" +
+            "product_id," +
+            "shop_category," +
+            "name," +
+            "price" +
+            ")" +
+            "values (" + shopDataModel.product_id + ", \"" + shopDataModel.shop_category + "\", \"" + shopDataModel.name + "\", " + shopDataModel.price + ")";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード取得
+    public static ShopDataModel Select()
+    {
+        string query = "select * from shop_data";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        DataTable dataTable = sqlDB.ExecuteQuery(query);
+        ShopDataModel shopDataModel = new ShopDataModel();
+        foreach (DataRow record in dataTable.Rows)
+        {
+            shopDataModel.product_id    = int.Parse(record["product_id"].ToString());
+            shopDataModel.shop_category = record["shop_category"].ToString();
+            shopDataModel.name          = record["name"].ToString();
+            shopDataModel.price         = int.Parse(record["price"].ToString());
+        }
+        return shopDataModel;
+    }
+}

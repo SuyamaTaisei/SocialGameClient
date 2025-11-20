9ce24e2d04dbc3cccfd8bd14d5dbd7c6e9020fdc
SuyamaTaisei <artistplain@gmail.com>
2025-11-20 20:25:11 +0900

    [#10574] インスタンスの実装(キャラクター、アイテム)
    ・キャラクターマスタデータ用のテーブルファイルを作成

---
+++ Assets/Scripts/Tables/CharacterDataTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class CharacterDataModel
+{
+    public int id;
+    public int rarity_id;
+    public int character_category;
+    public string name;
+}
+
+public class CharacterDataTable
+{
+    //テーブル作成
+    public static void CreateTable()
+    {
+        string query = "create table if not exists character_data(" +
+            "id int," +
+            "rarity_id int," +
+            "character_category int," +
+            "name string," +
+            "primary key(id))";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード挿入
+    public static void Insert(CharacterDataModel[] characterDataModel)
+    {
+        foreach (CharacterDataModel item in characterDataModel)
+        {
+            string query = "insert or replace into character_data (" +
+                "id," +
+                "rarity_id," +
+                "character_category," +
+                "name" +
+                ")" +
+                "values (" + item.id + ", " + item.rarity_id + ", " + item.character_category + ", \"" + item.name + "\")";
+            SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+            sqlDB.ExecuteNonQuery(query);
+        }
+    }
+}
---
+++ Assets/Scripts/Tables/CharacterRaritiesTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class CharacterRaritiesModel
+{
+    public int id;
+    public string name;
+}
+
+public class CharacterRaritiesTable
+{
+    //テーブル作成
+    public static void CreateTable()
+    {
+        string query = "create table if not exists character_rarities(" +
+            "id int," +
+            "name string," +
+            "primary key(id))";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード挿入
+    public static void Insert(CharacterRaritiesModel[] characterRaritiesModel)
+    {
+        foreach (CharacterRaritiesModel item in characterRaritiesModel)
+        {
+            string query = "insert or replace into character_rarities (" +
+                "id," +
+                "name" +
+                ")" +
+                "values (" + item.id + ", \"" + item.name + "\")";
+            SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+            sqlDB.ExecuteNonQuery(query);
+        }
+    }
+}

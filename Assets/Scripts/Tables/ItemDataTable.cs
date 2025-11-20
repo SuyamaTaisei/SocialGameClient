2fd2c6bd7496cd7bdebb65bc677b8882277edd39
SuyamaTaisei <artistplain@gmail.com>
2025-11-20 20:26:43 +0900

    [#10574] インスタンスの実装(キャラクター、アイテム)
    ・アイテムマスタデータ用のテーブルファイルを作成

---
+++ Assets/Scripts/Tables/ItemDataTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class ItemDataModel
+{
+    public int id;
+    public int rarity_id;
+    public int item_category;
+    public string name;
+    public string description;
+    public int value;
+}
+
+public class ItemDataTable
+{
+    //テーブル作成
+    public static void CreateTable()
+    {
+        string query = "create table if not exists item_data(" +
+            "id int," +
+            "rarity_id int," +
+            "item_category int," +
+            "name string," +
+            "description string," +
+            "value int," +
+            "primary key(id))";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード挿入
+    public static void Insert(ItemDataModel[] itemDataModel)
+    {
+        foreach (ItemDataModel item in itemDataModel)
+        {
+            string query = "insert or replace into item_data (" +
+                "id," +
+                "rarity_id," +
+                "item_category," +
+                "name," +
+                "description," +
+                "value" +
+                ")" +
+                "values (" + item.id + ", " + item.rarity_id + ", " + item.item_category + ", \"" + item.name + "\", \"" + item.description + "\", " + item.value + ")";
+            SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+            sqlDB.ExecuteNonQuery(query);
+        }
+    }
+}
---
+++ Assets/Scripts/Tables/ItemRaritiesTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class ItemRaritiesModel
+{
+    public int id;
+    public string name;
+}
+
+public class ItemRaritiesTable
+{
+    //テーブル作成
+    public static void CreateTable()
+    {
+        string query = "create table if not exists item_rarities(" +
+            "id int," +
+            "name string," +
+            "primary key(id))";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード挿入
+    public static void Insert(ItemRaritiesModel[] itemRaritiesModel)
+    {
+        foreach (ItemRaritiesModel item in itemRaritiesModel)
+        {
+            string query = "insert or replace into item_rarities (" +
+                "id," +
+                "name" +
+                ")" +
+                "values (" + item.id + ", \"" + item.name + "\")";
+            SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+            sqlDB.ExecuteNonQuery(query);
+        }
+    }
+}

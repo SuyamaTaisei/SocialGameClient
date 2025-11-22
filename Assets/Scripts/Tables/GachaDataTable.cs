40cc4de6d83e02d92d6f0280b162e524ea1d0a2f
SuyamaTaisei <artistplain@gmail.com>
2025-11-23 00:02:20 +0900

    [#10577] ガチャ画面の実装
    ・ガチャ期間、ガチャデータ用のテーブルファイルを作成

---
+++ Assets/Scripts/Tables/GachaDataTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+[Serializable]
+public class GachaDataModel
+{
+    public int gacha_id;
+    public int character_id;
+    public int weight;
+}
+
+public class GachaDataTable
+{
+    //テーブル作成
+    public static void CreateTable()
+    {
+        string query = "create table if not exists gacha_data(" +
+            "gacha_id int," +
+            "character_id int," +
+            "weight int," +
+            "primary key(character_id))";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード挿入
+    public static void Insert(GachaDataModel[] gachaDataModel)
+    {
+        foreach (GachaDataModel item in gachaDataModel)
+        {
+            string query = "insert or replace into gacha_data (" +
+                "gacha_id," +
+                "character_id," +
+                "weight" +
+                ")" +
+                "values (" + item.gacha_id + ", " + item.character_id + ", " + item.weight + ")";
+            SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+            sqlDB.ExecuteNonQuery(query);
+        }
+    }
+}
---
+++ Assets/Scripts/Tables/GachaPeriodsTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+[Serializable]
+public class GachaPeriodsModel
+{
+    public int id;
+    public string name;
+    public int single_cost;
+    public int multi_cost;
+    public string start;
+    public string end;
+}
+
+public class GachaPeriodsTable
+{
+    //テーブル作成
+    public static void CreateTable()
+    {
+        string query = "create table if not exists gacha_periods(" +
+            "id int," +
+            "name string," +
+            "single_cost int," +
+            "multi_cost int," +
+            "start string," +
+            "end string," +
+            "primary key(id))";
+        SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+        sqlDB.ExecuteNonQuery(query);
+    }
+
+    //レコード挿入
+    public static void Insert(GachaPeriodsModel[] gachaPeriodsModel)
+    {
+        foreach (GachaPeriodsModel item in gachaPeriodsModel)
+        {
+            string query = "insert or replace into gacha_periods (" +
+                "id," +
+                "name," +
+                "single_cost," +
+                "multi_cost," +
+                "start," +
+                "end" +
+                ")" +
+                "values (" + item.id + ", \"" + item.name + "\", " + item.single_cost + ", " + item.multi_cost + ", \"" + item.start + "\", \"" + item.end + "\")";
+            SqliteDatabase sqlDB = new SqliteDatabase(GameUtility.Const.SQLITE_DB_NAME);
+            sqlDB.ExecuteNonQuery(query);
+        }
+    }
+}

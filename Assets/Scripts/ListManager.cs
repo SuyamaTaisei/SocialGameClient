1518b7db04b9a93b8a5255cbbb78b2a439341c29
SuyamaTaisei <artistplain@gmail.com>
2025-11-28 00:32:44 +0900

    [#10577] ガチャ画面の実装
    ・ガチャ画面のガチャ種類リストの生成に応じて、リスト用のスクリプトを汎用化

--- Assets/Scripts/ListManager.cs
+++ Assets/Scripts/ListManager.cs
@@ -2,11 +2,12 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShopList : MonoBehaviour
+public class ListManager : MonoBehaviour
 {
     [SerializeField] Transform content;
     [SerializeField] GameObject templateView;
     [SerializeField] ClientShop clientShop;
+    [SerializeField] ClientGacha clientGacha;
 
     [SerializeField] int startCount;
     [SerializeField] int maxCount;
@@ -15,28 +16,27 @@ public class ShopList : MonoBehaviour
 
     private void Start()
     {
-        List<ShopDataModel> list = ShopDataTable.SelectAll();
+        List<ShopDataModel> shopList = ShopDataTable.SelectAll();
 
-        for (int i = startCount; i < list.Count && i <= maxCount; i++)
+        for (int i = startCount; i <= maxCount; i++)
         {
+            //雛形をもとにリストを生成
             GameObject item = Instantiate(templateView, content);
 
             //ボタンの取得
             Button button = item.GetComponentInChildren<Button>();
 
-            //選択されたボタンの番号をセット(index1は現金、index1とindex2はコインとジェム)
+            //選択されたボタンの番号をセット
             int index1 = product_id1 + i;
             int index2 = product_id2 + i;
 
             //生成されたリストから商品名や価格をセットする
             ShopItemView view = item.GetComponent<ShopItemView>();
-            if (view != null)
-            {
-                view.Set(list[i]);
-            }
+            if (view != null) view.Set(shopList[i]);
 
             //ボタン押下処理
-            button.onClick.AddListener(() => clientShop.OpenConfirmButton(index1, index2));
+            if (clientShop) button.onClick.AddListener(() => clientShop.OpenConfirmButton(index1, index2));
+            if (clientGacha) button.onClick.AddListener(() => clientGacha.GachaPeriodList(index1));
         }
     }
 }

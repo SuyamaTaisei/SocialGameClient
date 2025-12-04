3f3ce9743525df4c52620185862bc924a2ceae13
SuyamaTaisei <artistplain@gmail.com>
2025-12-04 16:13:53 +0900

    [#10577] ガチャ画面の実装
    ・ガチャピックアップリスト生成用クラスの実装
    ・ガチャピックアップのView用クラスの実装

---
+++ Assets/Scripts/List/GachaPickUpList.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GachaPickUpList : MonoBehaviour
+{
+    [SerializeField] Transform content;
+    [SerializeField] GameObject templateView;
+    [SerializeField] ClientGacha clientGacha;
+
+    [SerializeField] int startCount;
+    [SerializeField] int maxCount;
+    [SerializeField] int index;
+
+    private void Start()
+    {
+        for (int i = startCount; i <= maxCount; i++)
+        {
+            GameObject item = Instantiate(templateView, content);
+            int index = this.index + i;
+            var view = item.GetComponent<GachaPickUpItemView>();
+            if (clientGacha) clientGacha.ShowGachaUI(view, index);
+        }
+    }
+}
---
+++ Assets/Scripts/View/GachaPickUpItemView.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GachaPickUpItemView : MonoBehaviour
+{
+    [SerializeField] Image characterImage;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI rarityText;
+
+    public Image CharacterImage => characterImage;
+    public TextMeshProUGUI NameText => nameText;
+    public TextMeshProUGUI RarityText => rarityText;
+}

8eef48c1418c7f69064202a48e0c828d86ae2d4a
SuyamaTaisei <artistplain@gmail.com>
2025-12-06 18:33:46 +0900

    [#10578] ガチャ結果画面の実装
    ・ガチャ結果表示用のテンプレートViewクラスに、ガチャ報酬表示用のレイアウトを追加

--- Assets/Scripts/View/GachaResultTempView.cs
+++ Assets/Scripts/View/GachaResultTempView.cs
@@ -9,8 +9,20 @@ public class GachaResultTempView : MonoBehaviour
     [SerializeField] TextMeshProUGUI rarityText;
     [SerializeField] TextMeshProUGUI newText;
 
+    [SerializeField] Image itemImage;
+    [SerializeField] TextMeshProUGUI itemNameText;
+    [SerializeField] TextMeshProUGUI itemRarityText;
+    [SerializeField] TextMeshProUGUI itemAmountText;
+    [SerializeField] GameObject itemOtherObject;
+
     public Image CharacterImage => characterImage;
     public TextMeshProUGUI NameText => nameText;
     public TextMeshProUGUI RarityText => rarityText;
     public TextMeshProUGUI NewText => newText;
+
+    public Image ItemImage => itemImage;
+    public TextMeshProUGUI ItemNameText => itemNameText;
+    public TextMeshProUGUI ItemRarityText => itemRarityText;
+    public TextMeshProUGUI ItemAmountText => itemAmountText;
+    public GameObject ItemOtherObject => itemOtherObject;
 }

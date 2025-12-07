3f6fc1c1719abe6042e18aee7c9d21014b6be26f
SuyamaTaisei <artistplain@gmail.com>
2025-12-07 21:12:56 +0900

    [#10580] ガチャ提供割合画面の実装
    ・ガチャ提供割合表示用リストの実装

---
+++ Assets/Scripts/List/GachaOfferRateList.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GachaOfferRateList : MonoBehaviour
+{
+    [SerializeField] Transform content;
+    [SerializeField] GameObject templateView;
+    [SerializeField] ClientGacha clientGacha;
+
+    [SerializeField] int startCount;
+    [SerializeField] int maxCount;
+    [SerializeField] int index;
+
+    private void Start()
+    {
+        //提供割合表示を切り替えるために、Updateで何かしらの条件をもとに随時更新する必要がある
+        for (int i = startCount; i <= maxCount; i++)
+        {
+            GameObject item = Instantiate(templateView, content);
+            int index = this.index + i;
+            var view = item.GetComponent<GachaOfferRateTempView>();
+            if (clientGacha) clientGacha.ShowGachaOfferRateList(view, index);
+        }
+    }
+}
---
+++ Assets/Scripts/View/GachaOfferRateTempView.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GachaOfferRateTempView : MonoBehaviour
+{
+    [SerializeField] Image characterImage;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI rarityText;
+    [SerializeField] TextMeshProUGUI rateText;
+
+    public Image CharacterImage => characterImage;
+    public TextMeshProUGUI NameText => nameText;
+    public TextMeshProUGUI RarityText => rarityText;
+    public TextMeshProUGUI RateText => rateText;
+}

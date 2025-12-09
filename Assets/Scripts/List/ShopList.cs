ece6dd682d3f1ecf92c36e59a6a6233b15743910
SuyamaTaisei <artistplain@gmail.com>
2025-12-09 18:50:46 +0900

    [#10572] 販売アイテム一覧の実装
    ・ショップ画面にて、販売アイテム一覧の各画像の取得処理の実装

--- Assets/Scripts/List/ShopList.cs
+++ Assets/Scripts/List/ShopList.cs
@@ -16,25 +16,28 @@ public class ShopList : MonoBehaviour
     private void Start()
     {
         List<ShopDataModel> shopList = ShopDataTable.SelectAll();
+        int imageIndex = 1001;
 
         for (int i = startCount; i <= maxCount; i++)
         {
-            //雛形をもとにリストを生成
-            GameObject item = Instantiate(templateView, content);
-
-            //ボタンの取得
-            Button button = item.GetComponentInChildren<Button>();
-
             //選択されたボタンの番号をセット
             int index1 = this.index1 + i;
             int index2 = this.index2 + i;
 
-            //生成されたリストから商品名や価格をセットする
-            ShopTempView view = item.GetComponent<ShopTempView>();
-            if (view != null) view.Set(shopList[i]);
+            //雛形をもとにリストを生成
+            GameObject item = Instantiate(templateView, content);
 
-            //ボタン押下処理
-            if (clientShop) button.onClick.AddListener(() => clientShop.OpenConfirmButton(index1, index2));
+            Button button = item.GetComponentInChildren<Button>();
+            ShopTempView view = item.GetComponent<ShopTempView>();
+
+            //キャプチャしてから渡す
+            int itemId = imageIndex;
+            string imagePath = $"Images/Items/{itemId}";
+
+            //処理
+            if (clientShop) button.onClick.AddListener(() => clientShop.OpenConfirmButton(index1, index2, itemId));
+            if (view != null) view.Set(shopList[i], imagePath);
+            imageIndex++;
         }
     }
 }
--- Assets/Scripts/View/ShopTempView.cs
+++ Assets/Scripts/View/ShopTempView.cs
@@ -1,13 +1,16 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopTempView : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI productNameText;
-    [SerializeField] private TextMeshProUGUI priceText;
+    [SerializeField] Image itemImage;
+    [SerializeField] TextMeshProUGUI productNameText;
+    [SerializeField] TextMeshProUGUI priceText;
 
-    public void Set(ShopDataModel data)
+    public void Set(ShopDataModel data, string imagePath)
     {
+        if (itemImage) itemImage.sprite = Resources.Load<Sprite>(imagePath);
         if (productNameText) productNameText.text = data.name;
         if (priceText) priceText.text = data.price.ToString() + GameUtility.Const.SHOW_YEN;
     }

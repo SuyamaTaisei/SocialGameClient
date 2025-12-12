ee96f47d8b788e54aaf66982acf4ab22cc887081
SuyamaTaisei <artistplain@gmail.com>
2025-12-12 18:13:36 +0900

    [#10582] ガチャ履歴画面の実装
    ・ガチャ履歴用のリスト実装
    ・ガチャ履歴用のテンプレートViewの実装

---
+++ Assets/Scripts/List/GachaLogList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GachaLogList : MonoBehaviour
+{
+    [SerializeField] Transform content;
+    [SerializeField] GameObject templateView;
+
+    //DBモデル
+    private GachaPeriodsModel gachaPeriodsModel;
+    private CharacterDataModel characterDataModel;
+    private CharacterRaritiesModel characterRaritiesModel;
+
+    private void Start()
+    {
+        List<GachaLogsModel> gachaLogsList = GachaLogsTable.SelectAll();
+
+        for (int i = 0; i < gachaLogsList.Count; i++)
+        {
+            GameObject item = Instantiate(templateView, content);
+            var view = item.GetComponent<GachaLogTempView>();
+            int index = i;
+
+            characterDataModel = CharacterDataTable.SelectId(gachaLogsList[index].character_id);
+            characterRaritiesModel = CharacterRaritiesTable.SelectId(characterDataModel.rarity_id);
+            gachaPeriodsModel = GachaPeriodsTable.SelectId(gachaLogsList[index].gacha_id);
+            string imagePath = $"{GameUtility.Const.FOLDER_NAME_IMAGES}/{GameUtility.Const.FOLDER_NAME_CHARACTERS}/{gachaLogsList[index].character_id}";
+
+            //表記
+            view.CharacterImage.sprite = Resources.Load<Sprite>(imagePath);
+            view.NameText.text = characterDataModel.name;
+            view.RarityText.text = characterRaritiesModel.name;
+            view.DateTimeText.text = gachaLogsList[i].created_at;
+            view.GachaPeriodText.text = gachaPeriodsModel.name;
+        }
+    }
+}
---
+++ Assets/Scripts/View/GachaLogTempView.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GachaLogTempView : MonoBehaviour
+{
+    [SerializeField] Image characterImage;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI rarityText;
+    [SerializeField] TextMeshProUGUI dateTimeText;
+    [SerializeField] TextMeshProUGUI gachaPeriodText;
+
+    public Image CharacterImage => characterImage;
+    public TextMeshProUGUI NameText => nameText;
+    public TextMeshProUGUI RarityText => rarityText;
+    public TextMeshProUGUI DateTimeText => dateTimeText;
+    public TextMeshProUGUI GachaPeriodText => gachaPeriodText;
+}

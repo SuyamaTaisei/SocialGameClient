aa730799348b5d4471bd00688825407860888aa7
SuyamaTaisei <artistplain@gmail.com>
2026-01-01 12:15:29 +0900

    [#11212] キャラ強化画面の実装
    ・キャラクター強化画面にて、全強化アイテムだけを表示する一覧の実装
    ・強化アイテム一覧に表示する強化アイテム用のテンプレートViewを実装。強化ボタン押下時の処理を追加

---
+++ Assets/Scripts/Lists/EnhanceItemList.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnhanceItemList : MonoBehaviour
+{
+    [SerializeField] Transform content;
+    [SerializeField] GameObject templateView;
+    [SerializeField] ClientInstance clientInstance;
+
+    private void OnEnable() => RefreshEnhanceList();
+    private void OnDisable() => Clear();
+
+    private void Start()
+    {
+        RefreshEnhanceList();
+    }
+
+    private void RefreshEnhanceList()
+    {
+        List<ItemInstancesModel> itemInstancesList = ItemInstacesTable.SelectEnhanceItemAll();
+
+        //何もアイテムを所持していなければ
+        if (itemInstancesList == null || itemInstancesList.Count == 0)
+        {
+            clientInstance.NothingEnhanceItemMessage(GameUtility.Const.SHOW_INSTANCE_ENHANCE_ITEM_NOTHING);
+            return;
+        }
+
+        clientInstance.NothingEnhanceItemMessage("");
+
+        for (int i = 0; i < itemInstancesList.Count; i++)
+        {
+            //データの生成
+            GameObject item = Instantiate(templateView, content);
+            var view = item.GetComponent<EnhanceItemTemplateView>();
+
+            //データの取得
+            int index = i;
+            var data = itemInstancesList[index];
+            string imagePath = $"{GameUtility.Const.FOLDER_NAME_IMAGES}/{GameUtility.Const.FOLDER_NAME_ITEMS}/{data.item_id}";
+            ItemDataModel data1 = ItemDataTable.SelectId(data.item_id);
+            ItemRaritiesModel data2 = ItemRaritiesTable.SelectId(data1.rarity_id);
+
+            //データの描画
+            view.Set(data1, data2, data, imagePath);
+        }
+    }
+
+    //閉じてリセット
+    private void Clear()
+    {
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+}

297fce361033159b5c1ef57ca9b338dc60f3562b
SuyamaTaisei <artistplain@gmail.com>
2026-01-16 13:10:20 +0900

    [#11375] ガチャ画面の修正
    ・ガチャホーム画面用Viewの作成
    ・ガチャ期間のテンプレート用Viewに実装していたガチャホーム画面の表記処理を移動

---
+++ Assets/Scripts/Views/GachaFixedView.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GachaFixedView : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI gachaPeriodTitle;
+    [SerializeField] TextMeshProUGUI gachaPeriodText;
+    [SerializeField] TextMeshProUGUI gachaSingleCostText;
+    [SerializeField] TextMeshProUGUI gachaMultiCostText;
+    [SerializeField] TextMeshProUGUI gachaSingleText;
+    [SerializeField] TextMeshProUGUI gachaMultiText;
+
+    public void Set(GachaPeriodsModel gachaPeriodsModel, string periodEnd)
+    {
+        gachaPeriodTitle.text = gachaPeriodsModel.name;
+        gachaPeriodText.text = periodEnd;
+        gachaSingleCostText.text = gachaPeriodsModel.single_cost.ToString() + GameUtility.Const.SHOW_GEM;
+        gachaMultiCostText.text = gachaPeriodsModel.multi_cost.ToString() + GameUtility.Const.SHOW_GEM;
+        gachaSingleText.text = gachaPeriodsModel.single_count.ToString() + GameUtility.Const.SHOW_GACHA_COUNT;
+        gachaMultiText.text = gachaPeriodsModel.multi_count.ToString() + GameUtility.Const.SHOW_GACHA_COUNT;
+    }
+}

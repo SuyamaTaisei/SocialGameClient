7eba249ac74ef23e4f0da4d06704bcc33df33bc5
SuyamaTaisei <artistplain@gmail.com>
2026-02-08 20:38:30 +0900

    [#11362] ミッション受取確認画面の実装
    ・ミッション用のクライアント管理クラスの作成
    ・ミッション受け取り確認画面のリスト生成クラスの作成

---
+++ Assets/Scripts/Clients/ClientMission.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ClientMission : MonoBehaviour
+{
+    [SerializeField] Button missionInstanceOpenButton;
+    [SerializeField] Button missionInstanceCloseButton;
+    [SerializeField] GameObject missionInstanceView;
+
+    [SerializeField] InstanceMissionList instanceMissionList;
+    [SerializeField] InstanceMissionFixedView instanceMissionFixedView;
+    private ApiConnect apiConnect;
+
+    //ミッションidをキーにしたカテゴリを紐づけ
+    private readonly Dictionary<int, int> saveMission = new();
+    private const string column_id = "id";
+
+    private void Start()
+    {
+        apiConnect = ApiConnect.Instance;
+
+        missionInstanceView.SetActive(false);
+        missionInstanceOpenButton.onClick.AddListener(() =>
+        {
+            instanceMissionFixedView.SetCtrlAllReceivedButton(); //一括受取ボタン押下制御
+            missionInstanceView.SetActive(true);
+        });
+        missionInstanceCloseButton.onClick.AddListener(() => missionInstanceView.SetActive(false));
+    }
+
+    //ミッションid、カテゴリを保持
+    public void SaveMission(int id, int category)
+    {
+        saveMission[id] = category;
+    }
+
+    //保持された紐づけをリセット
+    public void ClearMission()
+    {
+        saveMission.Clear();
+    }
+
+    //ミッション受取の送信処理
+    public void RequestMissionReceived()
+    {
+        var usersModel = UsersTable.Select();
+
+        List<IMultipartFormSection> form = new()
+        {
+            new MultipartFormDataSection(column_id, usersModel.id),
+        };
+
+        //保持id、カテゴリでペアを生成
+        var mission = new List<KeyValuePair<int, int>>(saveMission);
+
+        //id、カテゴリをペアで送信して、URLの末尾に追加
+        for (int i = 0; i < mission.Count; i++)
+        {
+            //値の取り出し
+            var category = mission[i].Value;
+
+            form.Add(new MultipartFormDataSection($"mission[{i}][mission_id]", mission[i].Key.ToString()));
+            form.Add(new MultipartFormDataSection($"mission[{i}][mission_category]", category.ToString()));
+        }
+
+        //リクエスト送信後の成功時レスポンス受け取りコールバック
+        StartCoroutine(apiConnect.Send(GameUtility.Const.MISSION_RECEIVED_URL, form, (action) =>
+        {
+            instanceMissionList.Refresh();
+            instanceMissionFixedView.SetCtrlAllReceivedButton(); //一括受取ボタン押下制御
+            instanceMissionFixedView.SetConfirm(false);          //確認画面閉じる
+            instanceMissionFixedView.SetComplete(true);          //受取完了画面表示
+            ClearMission();
+        }));
+    }
+}
---
+++ Assets/Scripts/Lists/InstanceMissionConfirmList.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceMissionConfirmList : MonoBehaviour
+{
+    [SerializeField] Transform content;
+    [SerializeField] GameObject templateView;
+
+    private void OnDisable() => Clear();
+
+    //一括受取の場合
+    public void AllDataList(List<MissionInstancesModel> missionInstancesList)
+    {
+        Clear();
+        int imageIndex = 10001;
+
+        for (int i = 0; i < missionInstancesList.Count; i++)
+        {
+            //データの生成
+            GameObject item = Instantiate(templateView, content);
+            var view = item.GetComponent<InstanceMissionTemplateView>();
+
+            //データの取得
+            var data = missionInstancesList[i];
+            string imagePath = $"{GameUtility.Const.FOLDER_NAME_IMAGES}/{GameUtility.Const.FOLDER_NAME_GEMS}/{imageIndex}";
+            var data1 = MissionDataTable.SelectId(data.mission_id);
+
+            //データの描画
+            view.Set(data1, data, imagePath);
+        }
+    }
+
+    //単一受取の場合
+    public void SingleDataList(int missionId, int category)
+    {
+        var missionInstancesList = MissionInstancesTable.SelectIdAll(missionId, category);
+        AllDataList(missionInstancesList);
+    }
+
+    //閉じてリセット
+    private void Clear()
+    {
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+}

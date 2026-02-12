54e9969fdfaeaa6184ea32e49169fed0eaeae511
SuyamaTaisei <artistplain@gmail.com>
2026-02-12 16:15:14 +0900

    [#11363] オプション画面の実装
    ・オプション用固定画面クラスの作成

---
+++ Assets/Scripts/Views/OptionFixedView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionFixedView : MonoBehaviour
+{
+    [SerializeField] Button optionOpenButton;
+    [SerializeField] Button optionCloseButton;
+    [SerializeField] GameObject optionView;
+
+    private void Start()
+    {
+        optionView.SetActive(false);
+        optionOpenButton.onClick.AddListener(() => OptionOpenClose(true));
+        optionCloseButton.onClick.AddListener(() => OptionOpenClose(false));
+    }
+
+    public void OptionOpenClose(bool enabled)
+    {
+        optionView.SetActive(enabled);
+    }
+}

c7fec9aa12c055f37029b0273b8755e18477de54
SuyamaTaisei <artistplain@gmail.com>
2026-03-02 14:17:01 +0900

    [#11597] AWS用調整
    ・EC2用URLの変更

--- Assets/Scripts/Others/GameUtility.cs
+++ Assets/Scripts/Others/GameUtility.cs
@@ -9,7 +9,7 @@ namespace GameUtility
         #if ENV_LOCAL
             private const string BASE_URL = "http://localhost/";
         #elif ENV_DEV
-            private const string BASE_URL = "http://ec2-18-181-239-38.ap-northeast-1.compute.amazonaws.com/";
+            private const string BASE_URL = "http://ec2-52-195-240-136.ap-northeast-1.compute.amazonaws.com/";
         #endif
 
         //エンドポイント
